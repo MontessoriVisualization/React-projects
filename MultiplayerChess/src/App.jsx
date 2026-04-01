8ed2e8ecd9adbb76dbd1252af213893a274eab1e
MontessoriVisualization <MontessoriVisualization@users.noreply.github.com>
2026-04-01 16:37:14 +0545

    init

---
+++ MultiplayerChess/src/App.jsx
@@ -0,0 +1,10 @@
+import { Board } from "./component/Board";
+
+import React from 'react'
+
+ const App = () => {
+  return (
+<Board/>
+  )
+}
+export default App
---
+++ MultiplayerChess/src/component/Board.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { useState } from 'react'
+export const Board = () => {
+  
+const row= [1,2,3,4,5,6,7,8]
+const col= ['a','b','c','d','e','f','g','h']
+
+  return (
+    <>
+    <div className='board w-[600px] h-[600px] bg-green-300'></div>
+
+
+
+
+    </>
+
+
+)
+}
