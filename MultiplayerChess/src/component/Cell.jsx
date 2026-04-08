7575778268ecab828c307ea4c9c1278b5f2fd2d5
MontessoriVisualization <MontessoriVisualization@users.noreply.github.com>
2026-04-08 16:56:31 +0545

    chores finilize the boad an pices

--- MultiplayerChess/src/component/Cell.jsx
+++ MultiplayerChess/src/component/Cell.jsx
@@ -1,16 +1,40 @@
-import {
-  ChessBishop,
-  ChessKnight,
-  ChessPawn,
-  ChessQueen,
-  ChessRook,
-} from "lucide-react";
+import { motion } from "motion/react";
+
 const Cell = ({ value, isgreen, rowindex, colindex }) => {
+  const chessPieces = {
+    pawn: `https://assets-themes.chess.com/image/pxaxj/150/bp.png`,
+    king: `https://assets-themes.chess.com/image/pxaxj/150/bk.png`,
+    queen: `https://assets-themes.chess.com/image/pxaxj/150/bq.png`,
+    rook: `https://assets-themes.chess.com/image/pxaxj/150/br.png`,
+    bishop: `https://assets-themes.chess.com/image/pxaxj/150/bb.png`,
+    knight: `https://assets-themes.chess.com/image/pxaxj/150/bk.png`,
+  };
+
+  function initializePiece() {
+    if (colindex == 0 || colindex == 7) {
+      return (
+        <img className="w-full h-full p-2" src={chessPieces.rook} alt="rook" />
+      );
+    } else if (colindex == 1 || colindex == 6) {
+      return (
+        <img
+          className="w-full h-full p-2"
+          src={chessPieces.knight}
+          alt="knight"
+        />
+      );
+    }
+  }
+
+  const piece = initializePiece();
+
   return (
     <div
-      className={`relative bg-[#81b64c] w-full h-full ${isgreen ? "bg-[#81b64c] text-white" : "bg-white text-[#81b64c]"} text-center`}
+      className={`relative bg-[#739451] w-full h-full ${isgreen ? "bg-[#739451] text-white" : "bg-white text-[#81b64c]"} text-center`}
       value={value}
     >
+      {piece}
+
       {(rowindex == 0 || colindex == 7) && (
         <span className=" text-md font-semibold text-inherit">
           {colindex == 7 ? (
@@ -23,7 +47,6 @@ const Cell = ({ value, isgreen, rowindex, colindex }) => {
           ) : (
             ""
           )}
-          <ChessPawn className="w-full h-full p-1 fill-white text-white" />
         </span>
       )}
     </div>
